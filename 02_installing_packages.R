7ffa38b5a246db5d10f38988cde2e08e3e68a5af
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-03-07 17:50:03 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,9 @@
+# installing new packages in R
+
+install.packages("terra") # funzione per installare i pacchetti install.packages()
+# NB: ogni volta che prendiamo un pacchetto va sempre messo tra virgolette, il pacchetto viene preso da CRAN
+# se quando faccio sta roba qui mi chiede da quale città, devo scegliene una a caso
+# tramite la funzione library il pacchetto viene installato su R e controlla se è stato installato bene
+
+# se voglio rimuovere un pacchetto utilizzo: remove.packages()
+library(terra)
