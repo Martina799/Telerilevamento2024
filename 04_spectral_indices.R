762501dca4b14fb75b742281cdb0015dd438cd98
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-04-04 17:35:26 +0200

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -42,4 +42,35 @@ im.plotRGB(m2006, 1, 2, 3) # nir on R 2006
 im.plotRGB(m2006, 2, 1, 3) # nir on G 2006
 im.plotRGB(m2006, 2, 3, 1) # nir on B 2006
 
+# calculating the DVI (Difference vegetation index)
+dvi1992 = m1992[[1]] -m1992[[2]]  # uguale perchè è un'operazione matematica in cui prendo ogni singolo pixel della banda e lo sottraggo con 
+# il pixel della banda del rosso, sottraiamo quindi la banda numero 1 meno la banda numero 2
+# DVI varia da 255 a -255, questo perchè 8bit sono 256 pixel
+# la stessa formula si può utilizzare sapendo il nome delle bande: dvi1992=m1992$matagrosso-2219_lrg_1 - m199
+dvi1992
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
 
+dev.off() # e rifaccio rilancio il plot e mi ingrandisce l'immagine
+
+m2006 <-- im.import("matogrosso_ast_2006209_lrg.jpg") #faccio la stessa cosa con l'immagine del 2006
+# dvi 2006
+dvi2006 = m2006[[1]] - m2006[[2]]
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100) # il giallo è una delle radiazioni che si vede di più
+plot(dvi2006, col=cl)
+
+#esercizio: plot dvi1992 di fianco al dvi2006
+par(mfrow=c(1,2))
+plot(dvi1992, col=cl)
+plot(dvi2006, col=cl)
+
+#facciamo una normalizzazione --> NDVI (nir - red / nir + red). se il nir è max 255-0/255+0= 1 che è il max
+# se 0-255/0+255= -1, NDVI varia da 1 a -1
+# Normalized Difference Vegetation Index
+ndvi1992 = dvi1992 / (m1992[[1]]+m1992[[2]])
+ndvi2006 = dvi2006 / (m2006[[1]]+m2006[[2]])
+
+dev.off()
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)    
+plot(ndvi2006, col=cl) 
