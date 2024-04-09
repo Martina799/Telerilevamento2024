cbbc40a5b8f825e0ff3a8fdd68c9d752f107b539
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-04-09 17:42:13 +0200

    Update 05_classification.R

--- 05_classification.R
+++ 05_classification.R
@@ -1,8 +1,8 @@
 # quantifying land cover change --> copertura del suolo
 
 # installiamo nuovi pacchetti:
-# intsall.packages("ggplot2")
-# intsall.packages("patchwork")
+# install.packages("ggplot2")
+# install.packages("patchwork")
 
 library(terra)
 library(imageRy)
@@ -38,19 +38,22 @@ m2006c <- im.classify(m2006, num_clusters=2)
 # class 1 = human
 # class 2 = forest
 
-# frequencies --> frequnza: calcolo il numero di pixel di suolo o di foresta
+# Calculating frequencies --> frequnza: calcolo il numero di pixel di suolo o di foresta quindi per ogni classe
 f1992 <- freq(m1992c)
+f1992 # mi dà i valori: layer, value, count (numero di pixel)
 
-# proportions
-tot1992 <- ncell(m1992c)
-prop1992 = f1992 / tot1992
+# proportions --> proporzione numero pixel di una classe rispetto al totale
+tot1992 <- ncell(m1992c) # calcolo del totale
+tot1992
+prop1992 = f1992 / tot1992 #frequenza diviso tot
 
 # percentages
 perc1992 = prop1992 * 100
 
-# 17% human, 83% forest
+# risultati: 17% human, 83% forest
 
-# frequencies
+
+# frequencies del 2006
 f2006 <- freq(m2006c)
 
 # proportions
@@ -63,24 +66,29 @@ perc2006 = prop2006 * 100
 # 1992: 17% human, 83% forest
 # 2006: 55% human, 45% forest
 
-# let's build a dataframe
-class <- c("forest", "human")
-p1992 <- c(83, 17)
-p2006 <- c(45, 55)
 
-tabout <- data.frame(class, p1992, p2006)
+# let's build a dataframe --> CREIAMO I GRAFICI
+class <- c("forest", "human") # prima colonna della tabella: due classi forest e human (sono due vettori di un array vanno concatenati) in più vanno le virgolette perchè testo
+p1992 <- c(83, 17) # seconda colonna
+p2006 <- c(45, 55) # terza colonna
+
+tabout <- data.frame(class, p1992, p2006) # funzione che crea un dataframe --> tabella con classe (foresta/human), p1992 e p2006
 tabout
+View(tabout)
 
 # plotting the output
+# ggplot --> funzione
+# aes --> estetica del grafico
+# geom_bar --> geometria del grafico, in questo caso istogrammi. metto identity, ovvero il valore come lo abbiamo.
 ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
-ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white")
+ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white") # si vede come la parte umana è supera la foresta
 
-# patchwork
+# patchwork --> mette i grafici a confronto 
 p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
 p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white")
-p1 + p2
+p1 + p2 # NB --> il grafico viene con la scala sballata!!!!
 
-# varying axis and using lines
+# varying axis and using lines --> CORREZIONE --> ylim = limite della y da 0 a 100
 p1 <- ggplot(tabout, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(tabout, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1 + p2
