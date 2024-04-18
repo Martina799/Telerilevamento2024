0a75b5a2b1326aef75eee584d8aa3d652e18a79d
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-04-18 16:49:50 +0200

    Update 07_importing_external_data.R

--- 07_importing_external_data.R
+++ 07_importing_external_data.R
@@ -2,11 +2,32 @@
 
 library(terra)
 
-# set the working directory based on your path:
-# setwd("youtpath")
+# set the working directory based on your path: --> è la cartella dove stiamo lavorando, prendendo come esempio l'immagine dell'eclissi salvata (tasto dx e proprietà sull'immagine)
+# setwd("youtpath") --> funzione del set della cartella di lavoro
 # W***** users: C:\\path\Downloads -> C://path/Downloads
-# My own:
-setwd("~/Downloads")
+# My own: C:\Users\Martina\OneDrive - Alma Mater Studiorum Università di Bologna\Documents\UNIVERSITA'\TELERILEVAMENTO
+setwd("/Downloads") # esempio 
+setwd("C:/Users/Martina/OneDrive - Alma Mater Studiorum Università di Bologna/Documents/UNIVERSITA'/TELERILEVAMENTO") # dove ho salvato l'immagine dell'eclissi
+
+# carichiamo ora l'immagine dell'eclissi:
+eclissi <- rast("eclissi.png") # corrispondente di im.import() in imageRy, rast è una funzione di terra
+
+# plot data
+plotRGB(eclissi, 1, 2, 3) #R=1, G=2, B=3, se metto im.plot uso imageRy
+# plotRGB(eclissi, 2, 1, 3) plotto un'immagine con le bande invertite
+
+library(imageRy)
+im.plotRGB(eclissi, 1, 2, 3)
+dev.off()
+par(mfrow=c(1,2)) # metto le due immagini, usando terra e imageRy
+im.plotRGB(eclissi, 1, 2, 3)
+plotRGB(eclissi, 1, 2, 3)    
+
+# differenza tra banda 1 e 2
+dif = eclissi[[1]] - eclissi [[2]] # utile per fare delle analisi di differenza tra le due immagini
+plot(dif)
+
+# esempio del prof
 naja <- rast("najafiraq_etm_2003140_lrg.jpg")  # like in im.import()
 
 plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
