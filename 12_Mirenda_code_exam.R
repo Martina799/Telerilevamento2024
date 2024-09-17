862ba4967d2d3ef4aaed83c00b49fcc2295c8ae3
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-09-17 16:11:01 +0200

    Update 12_Mirenda_code_exam.R

--- 12_Mirenda_code_exam.R
+++ 12_Mirenda_code_exam.R
@@ -16,5 +16,16 @@ library(RNetCDF)
 # Setto la working directory per le immagini scelte
 setwd("C:/Users/Martina/OneDrive - Alma Mater Studiorum Università di Bologna/Documents/UNIVERSITA'/TELERILEVAMENTO")
 
-# Carico le immagini "true"
-giugno2022 <- rast("2022-06-01-True_color.jpg")
+# Carico le immagini "true color"
+apr2022 <- rast("2022-06-01-True_color.jpg")
+giu2022 <- rast("2022-06-01-True_color.jpg")
+set2022 <- rast("2022-06-01-True_color.jpg")
+ago2023 <- rast("2022-06-01-True_color.jpg")
+
+# Carico le immagini "false color"
+apr2022N <- rast("2022-06-01-True_color.jpg")
+giu2022N <- rast("2022-06-01-True_color.jpg")
+set2022N <- rast("2022-06-01-True_color.jpg")
+ago2023N <- rast("2022-06-01-True_color.jpg")
+
+plotRGB(giugno2022, 1, 2, 3)
