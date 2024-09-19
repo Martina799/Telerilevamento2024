35f4b2d0e290caa2ca7ac246c12d82d05352ee5f
Martina799 <162352416+Martina799@users.noreply.github.com>
2024-09-19 19:11:36 +0200

    Update 12_Mirenda_code_exam.R

--- 12_Mirenda_code_exam.R
+++ 12_Mirenda_code_exam.R
@@ -29,30 +29,42 @@ setwd("C:/Users/Martina/OneDrive - Alma Mater Studiorum Università di Bologna/D
 #ago2023N <- rast("2023-08-15-False_color.jpg")
 
 # Carico le immagini "true color" - AUSTRALIA
-mar2019 <- rast("2019-03-26-True_color.jpg")
-feb2020 <- rast("2020-02-04-True_color.jpg")
-gen2021 <- rast("2021-01-24-True_color.jpg")
-feb2024 <- rast("2024-02-13-True_color.jpg")
+#mar2019 <- rast("2019-03-26-True_color.jpg")
+#feb2020 <- rast("2020-02-04-True_color.jpg")
+#gen2021 <- rast("2021-01-24-True_color.jpg")
+#feb2024 <- rast("2024-02-13-True_color.jpg")
 
-# Carico le immagini "true color" - AUSTRALIA
-mar2019N <- rast("2019-03-26-False_color.jpg")
-feb2020N <- rast("2020-02-04-False_color.jpg")
-gen2021N <- rast("2021-01-24-False_color.jpg")
-feb2024N <- rast("2024-02-13-False_color.jpg")
+# Carico le immagini "false color" - AUSTRALIA
+#mar2019N <- rast("2019-03-26-False_color.jpg")
+#feb2020N <- rast("2020-02-04-False_color.jpg")
+#gen2021N <- rast("2021-01-24-False_color.jpg")
+#feb2024N <- rast("2024-02-13-False_color.jpg")
+
+# Carico le immagini "true color" - ISLANDA
+set2023 <- rast("2023-09-21-True_color.jpg")
+mar2024 <- rast("2024-03-02-True_color.jpg")
+lug2024 <- rast("2024-07-07-True_color.jpg")
+set2024 <- rast("2024-09-13-True_color.jpg")
+
+# Carico le immagini "false color" - ISLANDA
+set2023N <- rast("2023-09-21-False_color.jpg")
+mar2024N <- rast("2024-03-02-False_color.jpg")
+lug2024N <- rast("2024-07-07-False_color.jpg")
+set2024N <- rast("2024-09-13-False_color.jpg")
 
 # Plot delle 4 immagini True color in una griglia 2x2
 par(mfrow=c(2,2))
-plotRGB(mar2019, 1, 2, 3)
-plotRGB(feb2020, 1, 2, 3)
-plotRGB(gen2021, 1, 2, 3)
-plotRGB(feb2024, 1, 2, 3)
+plotRGB(set2023, 1, 2, 3)
+plotRGB(mar2024, 1, 2, 3)
+plotRGB(lug2024, 1, 2, 3)
+plotRGB(set2024, 1, 2, 3)
 
 # Plot delle 4 immagini NIR in una griglia 2x2
 par(mfrow=c(2,2))
-plotRGB(mar2019N, 1, 2, 3)
-plotRGB(feb2020N, 1, 2, 3)
-plotRGB(gen2021N, 1, 2, 3)
-plotRGB(feb2024N, 1, 2, 3)
+plotRGB(set2023N, 1, 2, 3)
+plotRGB(mar2024N, 1, 2, 3)
+plotRGB(lug2024N, 1, 2, 3)
+plotRGB(set2024N, 1, 2, 3)
 
 #feb2020N <- rast("2020-02-04-False_color.jpg")
 #plotRGB(feb2020N, 1, 2, 3)
@@ -60,22 +72,22 @@ plotRGB(feb2024N, 1, 2, 3)
 
 
 # DIFFERENZE
-# Quantifico i cambiamenti della vegetazione nel tempo tra le diverse immagini:
+# Quantifico i cambiamenti nell'estensione del campo di lava tra le diverse immagini nel tempo:
 
 #diffGA2022 <- giu2022N[[1]] - apr2022N[[1]]
 #diffSG2022 <- set2022N[[1]] - giu2022N[[1]]
 #diffAS2223 <- ago2023N[[1]] - set2022N[[1]]
 #diffPrePost <- set2022N[[1]] - ago2023N[[1]]
 #stackdiff <- c(diffGA2022, diffSG2022, diffAS2223,diffPrePost)
-difFM2019 <- feb2020N[[1]] - mar2019N[[1]]
-difGF2021 <- gen2021N[[1]] - feb2020N[[1]]
-difFG2421 <- feb2024N[[1]] - gen2021N[[1]]
-difeventoPost <- feb2024N[[1]] - feb2020N[[1]]
-stackdif <- c(difFM2019, difGF2021, difFG2421, difeventoPost)
+difMS2423 <- mar2024N[[1]] - set2023N[[1]]
+difLM2424 <- lug2024N[[1]] - mar2024N[[1]]
+difSL2424 <- set2024N[[1]] - lug2024N[[1]]
+difPD <- set2024N[[1]] - set2023N[[1]]
+stackdif <- c(difMS2423, difLM2424, difSL2424, difPD)
 
 # imposto la palette dei colori: prima per daltonici, seconda rosso, bianco e verde.
 viridisc <- colorRampPalette(viridis(7))(255)
-clg <- colorRampPalette(c("red", "white", "green")) (100)
+clg <- colorRampPalette(c("black", "orange", "yellow")) (100)
 #plot immagini differenze a colori
 plot(stackdif, col=clg)
 plot(stackdif, col=viridisc)
